f755f5cf2a9802dca9642d1ddc8f6c1d518f7f5c
Muratov Nizhat <nizha.777@mail.ru>
2017-11-24 11:24:08 +0600

    third commit

--- js/app.js
+++ js/app.js
@@ -5,6 +5,11 @@ app.config(function($routeProvider) {
         controller: 'samoletController'
     })
 
+    $routeProvider.when("/polosa", {
+        templateUrl: 'templates/polosa.html',
+        controller: 'polosaController'
+    })
+
     $routeProvider.otherwise({
         redirectTo: '/'
     });
@@ -33,9 +38,29 @@ app.controller('samoletController', function($scope,localStorageService) {
             return;
         }
 
-        
+
         reisi[reisIndex].addSamolet(new Samolet($scope.tip_Samoleta, $scope.bortNomer_Samoleta));
         $scope.message_Samolet = "Самолет добавлен в рейс " + $scope.reis_model + ".";
+    }
+
+})
+
+
+app.controller('polosaController', function($scope,localStorageService) {
+
+    document.getElementById("reisList").innerHTML = localStorageService.get('key');
+
+    $scope.createPolosa = function()
+    {
+        reisi.push(new Polosa($scope.nomerPolosi));
+
+        var options = "";
+        for (var i = 0; i < reisi.length; i++)
+            options += `<option value = ${reisi[i].getId()} />`;
+        document.getElementById("polosaList").innerHTML = options;
+        $scope.createPolosa_message = `Создана полоса - ${reisi[reisi.length - 1].getId()}`;
+
+        localStorageService.set('key', options);
     }
 
 })
@@ -43,7 +68,7 @@ app.controller('samoletController', function($scope,localStorageService) {
 var reisi = [];
 
 app.controller('AppController', function($scope,localStorageService) {
-    
+    document.getElementById("polosaList").innerHTML = localStorageService.get('key');
     $scope.addReis = function()
     {
 
@@ -68,19 +93,34 @@ app.controller('AppController', function($scope,localStorageService) {
         for (var i = 0; i < reisi.length; i++)
             if (reisi[i].getNomerReisa() === $scope.reis_model)
                 groupIndex = i;
-        $scope.tipSamoleta = reisi[groupIndex].getTipSamoleta();
-        $scope.bortNomer = reisi[groupIndex].getBortNomer();
+        console.log(JSON.stringify(reisi[groupIndex]));
+        var samoleti = reisi[groupIndex].getSamoleti();
+        var samIndex;
+        for(var i = 0; i < samoleti.length; i++) {
+            samIndex = i;
+        }
+        $scope.tipSamoleta = reisi[groupIndex].samoleti[samIndex].getTipSamoleta();
+        $scope.bortNomer = reisi[groupIndex].samoleti[samIndex].getBortNomer();
         $scope.samoletCounter = reisi[groupIndex].getSamoletCounter();
         $scope.aviaKompaniya = reisi[groupIndex].getAviaKompaniya();
 
         var list = "";
-        var samoleti = reisi[groupIndex].getSamoleti();
         for (var i = 0; i < samoleti.length; i++)
         {
             var s = samoleti[i];
             list += s.getId()+ " " + s.getTipSamoleta() + " " + s.getBortNomer() + "\n";
         }
         $scope.samoleti = list;
+
+        $scope.status_type = "";
+        if($scope.status_type === "Подготовка"){
+            $scope.status_message = "Подготовка";
+        } else if ($scope.status_type === "Полет"){
+            $scope.status_message = "Полет";
+        } else if ($scope.status_type === "Выполнение") {
+            $scope.status_message = "Выполнение";
+        }
+
     }
 
 
@@ -89,47 +129,65 @@ app.controller('AppController', function($scope,localStorageService) {
 
 Reis.idCounter = 0;
 function Reis(nomer, aviaKomp, kolPas) {
-	var id = ++Reis.idCounter;
-	var nomerReisa = nomer;
-	var aviaKompaniya = aviaKomp;
-	var kolPassazhirov = kolPas;
-	var samoleti = [];
-	var samoletCounter = 0;
+	this.id = ++Reis.idCounter;
+    this.nomerReisa = nomer;
+    this.aviaKompaniya = aviaKomp;
+    this.kolPassazhirov = kolPas;
+    this.samoleti = [];
+    this.samoletCounter = 0;
 
 
-	this.getId = function()	{return id;}
-	this.getNomerReisa = function() {return nomerReisa;}
-	this.setNomerReisa = function(value) { nomerReisa = value;}
-	this.getAviaKompaniya = function() { 	return aviaKompaniya;}
-	this.setAviaKompaniya = function(value) {	aviaKompaniya = value;}
-	this.getKolPassazhirov = function() { return kolPassazhirov;}
-	this.setKolPassazhirov = function(value){	kolPassazhirov = value;}
+	this.getId = function()	{return this.id;}
+	this.getNomerReisa = function() {return this.nomerReisa;}
+	this.setNomerReisa = function(value) { this.nomerReisa = value;}
+	this.getAviaKompaniya = function() { 	return this.aviaKompaniya;}
+	this.setAviaKompaniya = function(value) {	this.aviaKompaniya = value;}
+	this.getKolPassazhirov = function() { return this.kolPassazhirov;}
+	this.setKolPassazhirov = function(value){	this.kolPassazhirov = value;}
 
 	this.addSamolet = function(samolet) {
-		samoleti[samoletCounter] = samolet;
-		samoletCounter++;
+		this.samoleti[this.samoletCounter] = samolet;
+		this.samoletCounter++;
 	}
 
 	this.getSamoletCounter = function() {
-		return samoleti.length;
+		return this.samoleti.length;
 	}
 
 	this.getSamoleti = function () {
-        return samoleti;
+        return this.samoleti;
     }
+
 }
 
 Samolet.idCounter = 0;
 function Samolet(tipValue, nomerValue){
-	var id = ++Samolet.idCounter;
-	var tipSamoleta = tipValue;
-	var bortNomer = nomerValue;
-
-	this.getId = function () {  return id;}
-	this.getTipSamoleta = function()	{	return tipSamoleta;}
-	this.setTipSamoleta = function(value)	{	tipSamoleta = value; }
-	this.getBortNomer = function() {	return bortNomer;}
-	this.setBortNomer = function(value) {	bortNomer = value;}
+	this.id = ++Samolet.idCounter;
+	this.tipSamoleta = tipValue;
+	this.bortNomer = nomerValue;
+	this.getId = function () {  return this.id;}
+	this.getTipSamoleta = function()	{	return this.tipSamoleta;}
+	this.setTipSamoleta = function(value)	{	this.tipSamoleta = value; }
+	this.getBortNomer = function() {	return this.bortNomer;}
+	this.setBortNomer = function(value) {	this.bortNomer = value;}
 }
 
+Polosa.idCounter = 0;
+function Polosa(nomerVal) {
+    this.id = ++Polosa.idCounter;
+    this.nomerPolosi = nomerVal;
+    this.svoboden = new Boolean(true);
+
+    if(this.svoboden) {
+        console.log("Сваободен");
+    }
+    else {
+        console.log("Занят");
+    }
+
+
+    this.getId = function () {    return this.id;}
+    this.getNomerPolisi = function() {  return this.nomerPolosi;}
+    this.setNomerPolosi = function(value) {     this.nomerPolosi = value; }
+}
 
